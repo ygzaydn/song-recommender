7104c5522a314f284d253423ff4c9ba1f4b476f8
erolyagizaydin <32926423+ygzaydn@users.noreply.github.com>
2020-09-27 11:58:36 +0300

    redux initializations

---
+++ env-variables.js
@@ -0,0 +1,1 @@
+export const API_KEY = `0adb5651a8184ff0cdbc74bf4df52fcd`;
---
+++ src/store.js
@@ -0,0 +1,74 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { createLogger } from 'redux-logger'
+
+//action types
+const RECOMMEND_SONG = 'RECOMMEND_SONG';
+const GET_TRACK = 'GET_TRACK'
+
+//reducers
+const recommendSongReducer = (state = [], action) => {
+    switch(action.type) {
+        case RECOMMEND_SONG: {
+            applyRecommendSong(state, action)
+        }
+        default: return state
+    }
+}
+
+const songInfoStateReducer = (state = [], action) => {
+    switch(action.type) {
+        case GET_TRACK: {
+            return applyGetTrack(state, action)
+        }
+        default: return state;
+    }
+}
+
+//actions
+const applyRecommendSong = (state, action) => {
+    return {...action.similartracks}
+}
+
+const applyGetTrack = (state, action) => {
+    return {...action.track}
+}
+
+//action creators
+
+const doRecommendSong = (similartracks) => {
+    return {
+        type: RECOMMEND_SONG,
+        similartracks
+    }
+}
+
+const doGetTrack = (track) => {
+    return {
+        type: GET_TRACK,
+        track
+    }
+}
+
+//store initialization
+const logger = createLogger();
+
+const rootReducer = combineReducers({
+    recommendSongState: recommendSongReducer,
+    songInfoState: songInfoStateReducer,
+})
+export const store = createStore(rootReducer, undefined, applyMiddleware(logger));
+
+//React-redux functions
+export const mapStateToProps = (state) => {
+    return {
+        recommendSongState: state.recommendSongState,
+        songInfoState: state.songInfoState
+    }
+}
+
+export const mapDispatchToProps = (dispatch) => {
+    return {
+        onRecommendSong: similartracks => dispatch(doRecommendSong(similartracks)),
+        onGetTrack: track => dispatch(doGetTrack(track)),
+    }
+}
