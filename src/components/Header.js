c9001f02d55283d67948d812211e437581366725
ygz <ygzaydns@gmail.com>
2020-09-28 17:01:27 +0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Breadcrumbs, Link, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  ol: {
+    margin: '5%',
+    justifyContent: 'center',
+  },
+}));
 
 export const Header = ({handleClick}) => {
+    const classes = useStyles();
     return (
-    <Breadcrumbs aria-label="breadcrumb">
+    <Breadcrumbs classes={{ol: classes.ol}} aria-label="breadcrumb">
         <Link color="inherit" href="/" onClick={handleClick}>
             Material-UI
         </Link>
