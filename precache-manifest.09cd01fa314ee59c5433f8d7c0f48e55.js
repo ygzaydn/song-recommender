25ffe016d7069617a497783755ca7ec7aadd847d
erolyagizaydin <32926423+ygzaydn@users.noreply.github.com>
2020-09-29 23:28:02 +0300

    Updates

---
+++ precache-manifest.09cd01fa314ee59c5433f8d7c0f48e55.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfeb106219c478da992861b8176e1160",
+    "url": "/song-recommender/index.html"
+  },
+  {
+    "revision": "711d383287aaeada3cde",
+    "url": "/song-recommender/static/css/2.fea0eb96.chunk.css"
+  },
+  {
+    "revision": "4db00d961a0929f8f6b9",
+    "url": "/song-recommender/static/css/main.91b7bdfc.chunk.css"
+  },
+  {
+    "revision": "711d383287aaeada3cde",
+    "url": "/song-recommender/static/js/2.d734b261.chunk.js"
+  },
+  {
+    "revision": "afcffdf4ebf1780e0970711996cdd7dd",
+    "url": "/song-recommender/static/js/2.d734b261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db00d961a0929f8f6b9",
+    "url": "/song-recommender/static/js/main.8b2e0fd9.chunk.js"
+  },
+  {
+    "revision": "59d2261602141d9db63a",
+    "url": "/song-recommender/static/js/runtime-main.9632fda7.js"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/song-recommender/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/song-recommender/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/song-recommender/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/song-recommender/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/song-recommender/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/song-recommender/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/song-recommender/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/song-recommender/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/song-recommender/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/song-recommender/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/song-recommender/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/song-recommender/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/song-recommender/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/song-recommender/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/song-recommender/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/song-recommender/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
