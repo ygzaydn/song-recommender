1bc67ab05bdcbe13dda18df1122c7bd9d5d6f8ce
ygz <ygzaydns@gmail.com>
2020-09-30 11:27:58 +0300

    asd

--- src/components/Header-Component/Header.js
+++ src/components/Header-Component/Header.js
@@ -2,22 +2,37 @@ import React from 'react';
 import { Breadcrumbs, Link } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import './Header-style.css'
+import HomeIcon from '@material-ui/icons/Home';
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme) => ({
   ol: {
     justifyContent: 'center',
   },
   root: {
-      color: '#E5E5E5'
+      color: 'white'
+  },
+  icon: {
+      color: 'white',
+      marginRight: 'auto'
+  },
+  blankicon: {
+      color: 'black',
+      marginLeft: 'auto'
   }
 }));
 
 export const Header = ({handleClick, changeSection, classTrigger}) => {
     const classes = useStyles();
-    const headerClassName = classTrigger!=='' ? 'fixed-header' : ''
+    const headerClassName = classTrigger!=='' ? 'fixed-header' : '';
     return (
         <div className={headerClassName}>
-            <Breadcrumbs  classes={{ol: classes.ol}} aria-label="breadcrumb">
+            {headerClassName === 'fixed-header'
+            ? <IconButton className={classes.icon} href={`${window.location.href.split('#')[0]}`}>
+                <HomeIcon />
+              </IconButton>
+            : null}
+            <Breadcrumbs classes={{ol: classes.ol}} aria-label="breadcrumb">
                 <Link id="header" className={classes.root}color="inherit" href="#" onClick={handleClick('Artist')}>
                     Artists
                 </Link>
@@ -31,6 +46,9 @@ export const Header = ({handleClick, changeSection, classTrigger}) => {
                     Geo
                 </Link>
             </Breadcrumbs>
+            {headerClassName === 'fixed-header'
+            ? <HomeIcon className={classes.blankicon}/>
+            : null}
         </div>  
     )
 }
